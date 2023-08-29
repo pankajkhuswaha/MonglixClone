c88b480064fcce59422a4cc3a637144eb6381944
dev0869 <deveshbisht36@gmail.com>
2023-08-29 13:14:20 +0530

    asdsda

--- Mart/src/App.jsx
+++ Mart/src/App.jsx
@@ -2,18 +2,31 @@ import React, { useEffect } from "react";
 import Layout from "./layout/Layout";
 import Errorpage from "./pages/404/Errorpage";
 import { useSelector } from "react-redux";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Stack } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 import { getProducts } from "./features/ProductSlice";
 import { useDispatch } from "react-redux";
 import { Routess } from "./routes/Routes";
-const App = () => {
-  const { error } = useSelector((state) => state.products);
+import { VerifyApi } from "./features/authSlice";
 
+const App = () => {
   const dispatch = useDispatch();
+  const { error, success } = useSelector((state) => state.auth);
+
+  const verifyToken = () => {
+    dispatch(VerifyApi());
+
+    if (success) {
+      toast.success("sucesss");
+    } else if (error) {
+      toast.error("error");
+    }
+  };
+
   useEffect(() => {
+    verifyToken();
     dispatch(getProducts());
   }, [dispatch]);
 
