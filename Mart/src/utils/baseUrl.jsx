03a89a52c8d071162947b8e944f809be26309acb
pankajkhuswaha <khuswahapankaj00@gmail.com>
2023-09-25 18:14:08 +0530

    dgd

--- Mart/src/utils/baseUrl.jsx
+++ Mart/src/utils/baseUrl.jsx
@@ -1,2 +1,2 @@
-export const base_url = "http://192.168.1.4:7006/api/";
-// export const base_url = "http://eprocuretechbackend.deepmart.shop/api/";
+// export const base_url = "http://192.168.1.4:7006/api/";
+export const base_url = "http://eprocuretechbackend.deepmart.shop/api/";
