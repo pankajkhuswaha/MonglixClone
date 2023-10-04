a4afc4fc40c12527e4a6bba474ce495390a1d464
dev0869 <deveshbisht36@gmail.com>
2023-10-04 10:33:56 +0530

    AS

--- Mart/src/utils/baseUrl.jsx
+++ Mart/src/utils/baseUrl.jsx
@@ -1,2 +1,2 @@
-// export const base_url = "http://192.168.1.7:7006/api/";
-export const base_url = "https://eprocuretechbackend.deepmart.shop/api/";
+export const base_url = "http://127.0.0.1:7006/api/";
+// export const base_url = "https://eprocuretechbackend.deepmart.shop/api/";
