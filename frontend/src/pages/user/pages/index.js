136efb0feb83bdefa880357d1feddd80ce02136c
pankajkhuswaha <khuswahapankaj00@gmail.com>
2023-10-20 17:10:32 +0530

    solved

--- frontend/src/pages/user/pages/index.js
+++ frontend/src/pages/user/pages/index.js
@@ -2,7 +2,7 @@ import Shipping from "./Shipping";
 import Invoice from "./Invoice";
 import Order from './Order';
 import Payment from './Payment';
-import Profile from "./profile";
+import Profile from "./Profile";
 import UserMain from "./UserMain";
 export  {
     Shipping,
